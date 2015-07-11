96786e1ddcee2c08251ee9e29c82f0bbe0d555db
HappyLiuJian <liujianyousheng@gmail.com>
2015-07-11 23:47:03 +0800

    分数类

---
+++ src/course/Score.java
@@ -0,0 +1,34 @@
+package course;
+
+public class Score {
+	private int classId;
+	private int StudentId;
+	private String name;
+	private int score;
+	public int getClassId() {
+		return classId;
+	}
+	public void setClassId(int classId) {
+		this.classId = classId;
+	}
+	public int getStudentId() {
+		return StudentId;
+	}
+	public void setStudentId(int studentId) {
+		StudentId = studentId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+}
