1d21d4fd8582d3434d5a357c9d68f3b2fcb338a6
HappyLiuJian <liujianyousheng@gmail.com>
2015-07-11 23:47:39 +0800

    学生课程类

---
+++ src/course/sscore.java
@@ -0,0 +1,43 @@
+package course;
+
+public class sscore {
+	private String studentId;
+	private int courseId;
+	private String coursename;
+	private int credit;
+	private int score;
+	
+	
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	public String getCoursename() {
+		return coursename;
+	}
+	public void setCoursename(String coursename) {
+		this.coursename = coursename;
+	}
+	public int getCredit() {
+		return credit;
+	}
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	
+	
+}
