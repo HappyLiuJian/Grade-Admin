a8544c7608425f1c4f545b950c688f062345f103
HappyLiuJian <liujianyousheng@gmail.com>
2015-07-11 23:57:13 +0800

    教师类，存储教师信息

---
+++ src/user/Teacher.java
@@ -0,0 +1,25 @@
+package user;
+
+public class Teacher extends User{
+	private String sex;
+	private int age;
+	private String position;
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPosition() {
+		return position;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+}
